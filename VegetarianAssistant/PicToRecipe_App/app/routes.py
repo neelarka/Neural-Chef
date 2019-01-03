09042bd25d1b3904544340f535b8dcd99459e8c8
gcantu <39506132+cantugabriela@users.noreply.github.com>
2019-01-03 16:54:13 -0600

    Formatting CSS/HTML

---
+++ VegetarianAssistant/PicToRecipe_App/app/routes.py
@@ -0,0 +1,95 @@
+
+from flask import Flask, jsonify, render_template, url_for, request, redirect
+import json
+import os
+import glob
+from models import PredictRawVeggies
+#from bson import json_util
+# import app
+#from app import app
+# from app import retrieve_population_data
+#from app import models
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+my_pred = PredictRawVeggies()
+
+
+# # Define routes ###############################################################
+@app.route("/",  methods=['GET', 'POST'])
+def index():
+    print("upload click")
+    if request.method == 'POST':
+        print("POST click")
+        if request.files.get('file'):
+            print("File is there")
+#
+            images = request.files.getlist("file") #convert multidict to dict
+            print(f"Images: {images}")
+            # Remove all the files
+            files = glob.glob(app.config['UPLOAD_FOLDER']+'/*')
+            # print(files)
+            for f in files:
+                os.remove(f)
+
+            #save the image
+            for image in images:     #image will be the key
+                # create a path to the uploads folder
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+                image.save(filepath)
+                print(filepath)
+        return ('', 204)
+    # print(my_pred.labels)
+    return render_template('index.html')
+
+#####################################################################################
+@app.route('/update', methods=['POST'])
+def update():
+
+    print(f"In update {my_pred.labels}")
+    # get the list
+    filenames = []
+    image_names = request.form.getlist('image[]')
+
+    if (image_names == []):
+        filenames = ["No Images to predict"]
+        print("No Images to predict")
+    else:
+        for image_name in image_names:
+            filenames.append(image_name.split('\\')[-1:][0])
+
+        predictions = my_pred.call_predict(filenames, app.config['UPLOAD_FOLDER'])
+        # print(f"predictions : {predictions}")
+        # print(filenames)
+
+    return jsonify({'result': 'success', 'predictions': predictions})
+
+
+# Define routes ###############################################################
+# @app.route("/", methods=['GET', 'POST'])
+# def upload_file():
+#     data = {"success": False}
+#     if request.method == 'POST':
+#         print("In post")
+#         print("-------------------------------------")
+#
+#         if request.files.get('file'):
+#
+#             images = request.files.getlist("file") #convert multidict to dict
+#             for image in images:     #image will be the key
+#
+#                 # create a path to the uploads folder
+#                 filepath = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+#                 image.save(filepath)
+#                 print(filepath)
+#
+#         return ("", 204) #redirect(url_for('upload_file'))
+#     return render_template('index.html')
+
+
+# ###########################################################################
+# ###########################################################################
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ VegetarianAssistant/PicToRecipe_App/app/static/js/config.js
@@ -0,0 +1,7 @@
+// API key
+const MAPBOX_KEY = "pk.eyJ1IjoidHJ1cHRpc2FtYW50IiwiYSI6ImNqb3ZxeTJ2eTFwamQzdHBpN2pxMWRncm0ifQ.lSg57pODMZLvUx2AhOwALQ";
+const CARTO_API_KEY = "b48d03a14a86a80430d72ccbe74e3bf257ff3193";
+const USER_NAME = "truptisamant";
+const ZIP_ZOOM = 15;
+const STATE_ZOOM = 6;
+const US_ZOOM = 4;
